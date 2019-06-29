e869dc45fcb81dc6ff59483e14b7770b13597c1d
xyXuYang111 <1234qwer>
2019-06-30 02:03:08 +0800

    redis+aop模拟实现spring-session效果

--- src/main/java/com/xuyang/work/redis/listener/RedisMessageListener.java
+++ src/main/java/com/xuyang/work/redis/listener/RedisMessageListener.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
